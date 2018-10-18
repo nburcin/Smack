4aa2f1f2a14a09075a1be1d165f68dc6e2d4654b
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-18 11:33:07 -0400

    Added login view controller and built interface and segue.

---
+++ SmackTestChat/SmackTestChat/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  SmackTestChat
+//
+//  Created by Nicholas Burcin on 10/18/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ SmackTestChat/SmackTestChat/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SmackTestChat
+//
+//  Created by Nicholas Burcin on 10/18/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+
+let TO_LOGIN = "toLogin"
