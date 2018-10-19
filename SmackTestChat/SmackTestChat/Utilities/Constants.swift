d8288aaa0b63230a72daefb758330250f0ed8e0b
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-19 09:22:58 -0400

    added constants and alamofire implementation for user creation

--- SmackTestChat/SmackTestChat/Utilities/Constants.swift
+++ SmackTestChat/SmackTestChat/Utilities/Constants.swift
@@ -8,9 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL
+
+let BASE_URL = "https://chatty-mcchat-face.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
