49284bde97f7837eb43dcbd11b6b50e3605d3975
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-19 19:36:08 -0400

    Added Create user and unwind

--- SmackTestChat/SmackTestChat/Controller/CreateAccountVC.swift
+++ SmackTestChat/SmackTestChat/Controller/CreateAccountVC.swift
@@ -17,6 +17,12 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    // Variables
+    
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +31,7 @@ class CreateAccountVC: UIViewController {
     
 
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let password = passTxt.text , passTxt.text != "" else { return }
         
@@ -32,7 +39,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
                     if success {
-                        print( "User Logged In!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
