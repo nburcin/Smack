a696e2525d5d11481f59fd3ebb3f374d6b3c51ae
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-19 11:43:26 -0400

    Implemented SwiftyJSON

--- SmackTestChat/SmackTestChat/Services/AuthService.swift
+++ SmackTestChat/SmackTestChat/Services/AuthService.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Alamofire
-
+import SwiftyJSON
 
 class AuthService {
     
@@ -73,14 +73,20 @@ class AuthService {
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
-                if let json = response.result.value as? Dictionary<String, Any> {
-                    if let email = json["user"] as? String {
-                        self.userEmail = email
-                    }
-                    if let token = json["token"] as? String {
-                        self.authToken = token
-                    }
-                }
+//                if let json = response.result.value as? Dictionary<String, Any> {
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+                // Using SwiftyJSON
+                
+                guard let data = response.data else { return }
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
                 
                 self.isLoggedIn = true
                 completion(true)
