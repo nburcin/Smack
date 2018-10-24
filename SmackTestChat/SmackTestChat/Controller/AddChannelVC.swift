f1484d135458fb349cbf2904d74a4f29b5bf302f
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 06:38:29 -0400

    More xib addchannel updates

---
+++ SmackTestChat/SmackTestChat/Controller/AddChannelVC.swift
@@ -0,0 +1,28 @@
+//
+//  AddChannelVC.swift
+//  SmackTestChat
+//
+//  Created by Nicholas Burcin on 10/23/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    // Outlets
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+    }
+    
+}
