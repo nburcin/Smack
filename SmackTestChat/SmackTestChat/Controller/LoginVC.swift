cab7f5ff70fdd24c3e9f30f3a33326fd862cb3a1
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 08:44:12 -0400

    Added login functionality and corrected url constants for find user by email

--- SmackTestChat/SmackTestChat/Controller/LoginVC.swift
+++ SmackTestChat/SmackTestChat/Controller/LoginVC.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    // Outlets
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameTxt.text , usernameTxt.text != "" else { return }
+        guard let password = passwordTxt.text , passwordTxt.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (succcess) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
+    
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -24,4 +56,9 @@ class LoginVC: UIViewController {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    func setUpView() {
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+    }
 }
