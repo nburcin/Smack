4649f81397698898003064c2f89b71b8a9e5e55e
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-24 09:46:31 -0400

    established message service and channel model

---
+++ SmackTestChat/SmackTestChat/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  SmackTestChat
+//
+//  Created by Nicholas Burcin on 10/24/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
